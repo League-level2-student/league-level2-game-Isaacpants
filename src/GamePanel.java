8e38b74bb6d9cab30ea4ddd8a0083522ae1b0fed
League Student <league@iMac-22.attlocal.net>
2019-12-17 16:56:54 -0800

    game ip

---
+++ src/GamePanel.java
@@ -0,0 +1,31 @@
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements KeyListener {
+@Override
+public void paintComponent(Graphics g) {
+	g.fillRect(10,10, 100, 100);
+	
+}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/JumpingGame.java
@@ -0,0 +1,29 @@
+
+
+import javax.swing.JFrame;
+
+public class JumpingGame  {
+	static JFrame jf;
+	static GamePanel gp = new GamePanel();
+	public static int JG_WIDTH = 1000	;
+	public static int JG_HEIGHT = 700;
+	public JumpingGame() {
+		jf = new JFrame();
+	}public void setup() {
+		jf.add(gp);
+		jf.setSize(JG_WIDTH, JG_HEIGHT);
+		jf.addKeyListener(gp);
+		jf.setVisible(true);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+
+	}
+public static void main(String[] args) {
+	
+	JumpingGame jg = new JumpingGame();
+	jg.setup();
+	
+}
+
+}
