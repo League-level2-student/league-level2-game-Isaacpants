313257e9f3453a13a5bd65b71b22f97f4b3562d9
League Student <league@iMac-14.attlocal.net>
2020-01-07 16:54:55 -0800

    game ip

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,31 +1,63 @@
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel implements KeyListener {
-@Override
-public void paintComponent(Graphics g) {
-	g.fillRect(10,10, 100, 100);
+public class GamePanel extends JPanel implements MouseListener {
+	public static BufferedImage backgroundImg;
+	public GamePanel() {
+		try {
+			backgroundImg = ImageIO.read(this.getClass().getResourceAsStream("scrolling.png"));
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			
+		}
+		
+		
+	}
+	@Override
+	public void paintComponent(Graphics g) {
+	g.fillOval(50,50,50,50);
+		g.drawOval(50, 50, 50, 50);
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
-}
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
--- src/JumpingGame.java
+++ src/JumpingGame.java
@@ -1,33 +1,59 @@
 
 
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JFrame;
 
-public class JumpingGame  {
+
+public class JumpingGame implements MouseListener{
 	static JFrame jf;
 	static GamePanel gp = new GamePanel();
 	public static int JG_WIDTH = 1000	;
 	public static int JG_HEIGHT = 700;
+	
 	public JumpingGame() {
 		jf = new JFrame();
 	}public void setup() {
 		jf.add(gp);
 		jf.setSize(JG_WIDTH, JG_HEIGHT);
-		jf.addKeyListener(gp);
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.addMouseListener(gp);
 		
 		
-
 	}
 public static void main(String[] args) {
 	
-	//JumpingGame jg = new JumpingGame();
-	//jg.setup();
+	JumpingGame jg = new JumpingGame();
+	jg.setup();
+
+}
+@Override
+public void mouseClicked(MouseEvent e) {
+	// TODO Auto-generated method stub
 	
+}
+@Override
+public void mousePressed(MouseEvent e) {
+	// TODO Auto-generated method stub
 	
+}
+@Override
+public void mouseReleased(MouseEvent e) {
+	// TODO Auto-generated method stub
 	
+}
+@Override
+public void mouseEntered(MouseEvent e) {
+	// TODO Auto-generated method stub
 	
-	new Level2GameTemplate();
+}
+@Override
+public void mouseExited(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
 }
 
 
